213a1295f3d728837b213ebadcb14df602747142
Isaias Valle <guitartsword@gmail.com>
2016-03-22 00:58:10 -0600

    Added comments with a brief description of url

--- routes.js
+++ routes.js
@@ -3,11 +3,11 @@ var usersController = require('./controllers/usersController');
 var authController = require('./controllers/authController');
 
 exports.endpoints = [{method: 'GET', path: '/', config: {handler: function(request, reply){reply('API v1, Scores')}}},
-	{method: 'GET', path: '/v1/scores', config: scoresController.getScoresAll},
-	{method: 'GET', path: '/v1/scoresUser', config: scoresController.getScoresUser},
-	{method: 'POST', path: '/v1/score', config: scoresController.createScore},
-	{method: 'POST', path: '/v1/register', config: usersController.createUser},
-	{method: 'GET', path: '/v1/users', config: usersController.getAllUsers},
-	{method: 'POST', path: '/v1/login', config: authController.login},
-	{method: 'GET', path: '/v1/logout', config: authController.logout}
+	{method: 'GET', path: '/v1/scores', config: scoresController.getScoresAll}, //Obtiene todas las partituras en la base de datos
+	{method: 'GET', path: '/v1/scoresUser', config: scoresController.getScoresUser}, //Obtiene partituras del usuario con sesion inciada
+	{method: 'POST', path: '/v1/score', config: scoresController.createScore}, //Crea una partitura
+	{method: 'POST', path: '/v1/register', config: usersController.createUser}, //Crea(registra) un usuario
+	{method: 'GET', path: '/v1/users', config: usersController.getAllUsers}, //Obtiene todos los usuario
+	{method: 'POST', path: '/v1/login', config: authController.login}, //Un usuario inicia sesion
+	{method: 'GET', path: '/v1/logout', config: authController.logout} //Un usuario cierra sesion
 ];
